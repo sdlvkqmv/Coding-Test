2de113354aae6ce70cbb7f253b5455db437ecffe
신규철 <43338903+sdlvkqmv@users.noreply.github.com>
2026-04-03 23:20:39 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260403/DateTime to DateTime/datetime-to-datetime.py
+++ 260403/DateTime to DateTime/datetime-to-datetime.py
@@ -3,6 +3,6 @@ a, b, c = map(int, input().split())
 # Please write your code here.
 start_mins = 11 * 24 * 60 + 11 * 60 + 11
 
-end_mins = A * 24 * 60 + B * 60 + C
+end_mins = a * 24 * 60 + b * 60 + c
 
 print(start_mins - end_mins)
